797edf6871f324374de98f6edcba1ffabfa5500d
wastefello21-hub <wastefello21@gmail.com>
2026-06-12 10:46:48 +0530

    new update

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const nextConfig = {
   reactStrictMode: true,
-  typedRoutes: true,
+  experimental: {
+    webpackBuildWorker: false
+  },
   images: {
     remotePatterns: [
       {
@@ -16,7 +12,7 @@ const nextConfig = {
     ]
   },
   webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname);
+    config.cache = false;
     return config;
   }
 };
